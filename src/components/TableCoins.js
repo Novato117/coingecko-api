7de9f81e4900e8c668147ba9c6d999596df76461
Luis Fernando <93270513+Novato117@users.noreply.github.com>
2023-03-26 19:46:56 -0600

    Update TableCoins.js

--- src/components/TableCoins.js
+++ src/components/TableCoins.js
@@ -1,18 +1,18 @@
 import React from "react";
 const TableCoins = ({ coins }) => {
-
+const titles = ["#", "coin", "price", "price change", "24h volume"];
     return (
         <table className="table table-dark mt-4 table-hover">
             <thead>
                 <tr>
-                    <td>name</td>
+                     {titles.map((title) => (
+                        <td>{title}</td>
+                    ))}
                 </tr>
             </thead>
             <tbody>
-                {coins.map(coin => (
-                    <tr key={coin.name}>
-                        <td>{coin.name}</td>
-                    </tr>
+               {coins.map((coin, index) => (
+                    <CoinRow coin={coin} key={index} index={index + 1}></CoinRow>
                 ))}
             </tbody>
         </table>
