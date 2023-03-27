eb4de3693a0c527590d213a385485e2dd6414161
Luis Fernando <93270513+Novato117@users.noreply.github.com>
2023-03-26 19:45:22 -0600

    Update CoinRow.js

--- src/components/CoinRow.js
+++ src/components/CoinRow.js
@@ -1,32 +1,18 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-function CryptoPrices() {
-    const [prices, setPrices] = useState({})
-
-    const fetchPrices = async () => {
-        try {
-            const res = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,litecoin,moonbeam,moonriver,chainlink&vs_currencies=usd');
-            console.log(res.data)
-            setPrices(res.data)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    useEffect(() => {
-        const intervalId = setInterval(fetchPrices, 50000);
-        return () => clearInterval(intervalId)
-    }, []);
+function CoinRow({ coin, index }) {
+    console.log(coin, index)
     return (
-        <div>
-            <h2>cryptocurrency prices</h2>
-            <p> Bitcoin : {prices.bitcoin?.usd}</p>
-            <p>Ethereum: {prices.ethereum?.usd}</p>
-            <p>Litecoin: {prices.litecoin?.usd}</p>
-            <p>moonbeam: {prices.moonbeam?.usd}</p>
-            <p>moonriver: {prices.moonriver?.usd}</p>
-            <p>chainlink: {prices.chainlink?.usd}</p>
-            <p>precio producto: $5 usd : ${(5 / prices.moonbeam?.usd)} GLM</p>
-        </div>
+        <tr>
+            <td>{index}</td>
+            <td>
+                <img src={coin.image} alt={coin.name} style={{ width: '6%' }} className="me-4" />
+                <span>{coin.name}</span>
+                <span className="ms-3 text-muted">{coin.symbol}</span>
+            </td>
+            <td>{coin.current_price}</td>
+            <td>{coin.price_change_percentage_24h}</td>
+            <td>{coin.total_volume}</td>
+        </tr>
     )
 }
-export default CryptoPrices;
+
+export default CoinRow;
